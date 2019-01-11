be64c525a2fa5b2452ddf679837ab03c08021b29
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-11 18:35:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@
 def prime?(x)
   
   return false if x <= 1
-  Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+  Math.sqrt(x).floor.downto(2).each {|i| return false if x % i == 0}
    true
 end
